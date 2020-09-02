3ac1cbf62fb8218765c2393b24082dd2d8644561
renankanu <renankanu@gmail.com>
2020-09-02 13:15:45 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:app_kanu_delivery/src/screen/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kanu Delivery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
