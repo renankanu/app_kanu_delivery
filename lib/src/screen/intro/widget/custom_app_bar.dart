d7a86a94969f93271b57ee3081e8f5f5f94dfbef
renankanu <renankanu@gmail.com>
2020-09-06 10:52:30 -0300

    Feature - creta app bar IntroScreen

--- lib/src/screen/intro/widget/custom_app_bar.dart
+++ lib/src/screen/intro/widget/custom_app_bar.dart
@@ -4,31 +4,34 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.grey.withAlpha(50),
-              borderRadius: BorderRadius.all(
-                Radius.circular(16),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.grey.withAlpha(50),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16),
+                ),
               ),
+              child: Icon(Icons.arrow_back_ios),
             ),
-            child: Icon(Icons.arrow_back_ios),
-          ),
-          FlatButton(
-            onPressed: null,
-            child: Text(
-              "Skip",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 16),
-            ),
-          )
-        ],
+            FlatButton(
+              onPressed: null,
+              child: Text(
+                "Skip",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 16),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
