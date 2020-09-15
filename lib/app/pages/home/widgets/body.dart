853b6c50bdf0ab5dcb87556d7c2ee7fec98ec113
renankanu <renankanu@gmail.com>
2020-09-14 23:31:40 -0300

    Feature - create category list horizontal

--- lib/app/pages/home/widgets/body.dart
+++ lib/app/pages/home/widgets/body.dart
@@ -2,38 +2,55 @@ import 'package:app_kanu_delivery/app/constants.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
-  final List<String> numbers = ['Fast food', 'Fruits', 'Vegetable', 'Pizza'];
+  final List<String> numbers = [
+    'Fast food',
+    'Fruits',
+    'Vegetable',
+    'Pizza',
+    'Drinks',
+    'Fitness Food',
+    'Vegan'
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          "Let's eat\nQuality food",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            "Let's eat\nQuality food",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
         ),
-        Container(
-          height: 40,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: numbers.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: Card(
-                    color: kVictoria,
-                    child: Container(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                            child: Text(
-                          numbers[index],
-                          style: TextStyle(color: Colors.white),
-                        )),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Container(
+            height: 40,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: numbers.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: Card(
+                      color: kGreenPea,
+                      child: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                              child: Text(
+                            numbers[index],
+                            style: TextStyle(color: Colors.white),
+                          )),
+                        ),
                       ),
                     ),
-                  ),
-                );
-              }),
+                  );
+                }),
+          ),
         ),
       ],
     );
