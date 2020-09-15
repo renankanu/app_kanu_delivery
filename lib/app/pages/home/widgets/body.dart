87739d3edd6f71e0db87351d3397651fc77219ab
renankanu <renankanu@gmail.com>
2020-09-14 23:24:35 -0300

    Feature - create title style from HomeScreen

--- lib/app/pages/home/widgets/body.dart
+++ lib/app/pages/home/widgets/body.dart
@@ -1,29 +1,41 @@
+import 'package:app_kanu_delivery/app/constants.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
-  final List<String> numbers = ['Fast food', 'Pizza'];
+  final List<String> numbers = ['Fast food', 'Fruits', 'Vegetable', 'Pizza'];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 24.0),
-      height: 80,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: numbers.length,
-          itemBuilder: (context, index) {
-            return Container(
-              child: Card(
-                color: Colors.blue,
-                child: Container(
-                  child: Center(
-                      child: Text(
-                    numbers[index],
-                    style: TextStyle(color: Colors.white),
-                  )),
-                ),
-              ),
-            );
-          }),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Let's eat\nQuality food",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          height: 40,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: numbers.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Card(
+                    color: kVictoria,
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                            child: Text(
+                          numbers[index],
+                          style: TextStyle(color: Colors.white),
+                        )),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+        ),
+      ],
     );
   }
 }
