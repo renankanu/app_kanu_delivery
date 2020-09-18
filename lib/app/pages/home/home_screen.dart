7cddec45efb1b6dfc213c2cb9191b46f53d04361
renankanu <renankanu@gmail.com>
2020-09-18 14:35:22 -0300

    Install - bubble_bottom_bar

--- lib/app/pages/home/home_screen.dart
+++ lib/app/pages/home/home_screen.dart
@@ -1,60 +1,97 @@
-import 'package:app_kanu_delivery/app/constants.dart';
 import 'package:app_kanu_delivery/app/pages/home/widgets/body.dart';
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedItemIndex = 2;
+class HomeScreen extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    GestureDetector buildNavBarItem(IconData icon, int index) {
-      return GestureDetector(
-        onTap: () {
-          setState(() {
-            _selectedItemIndex = index;
-          });
-        },
-        child: Container(
-          width: MediaQuery.of(context).size.width / 5,
-          height: 60,
-          decoration: index == _selectedItemIndex
-              ? BoxDecoration(
-                  border:
-                      Border(bottom: BorderSide(width: 4, color: kGreenPea)),
-                  gradient: LinearGradient(colors: [
-                    kGreenPea.withOpacity(0.3),
-                    kGreenPea.withOpacity(0.016),
-                  ], begin: Alignment.bottomCenter, end: Alignment.topCenter))
-              : BoxDecoration(),
-          child: Icon(
-            icon,
-            color: index == _selectedItemIndex
-                ? kGreenPea
-                : Colors.black.withOpacity(0.6),
-          ),
-        ),
-      );
-    }
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State {
+  int currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentIndex = 0;
+  }
+
+  changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: SafeArea(
-        child: Row(
-          children: [
-            buildNavBarItem(Icons.search, 0),
-            buildNavBarItem(Icons.favorite_border, 1),
-            buildNavBarItem(Icons.home, 2),
-            buildNavBarItem(Icons.chat_bubble_outline, 3),
-            buildNavBarItem(Icons.person, 4),
+        bottomNavigationBar: BubbleBottomBar(
+          opacity: 0.2,
+          backgroundColor: Colors.white,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(16.0),
+          ),
+          currentIndex: currentIndex,
+          hasInk: true,
+          inkColor: Colors.black12,
+          hasNotch: true,
+          fabLocation: BubbleBottomBarFabLocation.end,
+          onTap: changePage,
+          items: [
+            BubbleBottomBarItem(
+              backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.dashboard,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.dashboard,
+                color: Colors.red,
+              ),
+              title: Text('Home'),
+            ),
+            BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.folder_open,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.folder_open,
+                color: Colors.indigo,
+              ),
+              title: Text('Folders'),
+            ),
+            BubbleBottomBarItem(
+              backgroundColor: Colors.deepPurple,
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.access_time,
+                color: Colors.deepPurple,
+              ),
+              title: Text('Log'),
+            ),
           ],
         ),
-      ),
-      body: SafeArea(
-        child: Body(),
-      ),
-    );
+        body: SafeArea(child: Body()));
   }
 }
