2b802c11d0c0f383c666946a3db5ae1cbd0bc0f6
renankanu <renankanu@gmail.com>
2020-09-20 00:30:26 -0300

    Refactor - back bottomNavigationBar

--- lib/app/pages/home/home_screen.dart
+++ lib/app/pages/home/home_screen.dart
@@ -1,97 +1,62 @@
+import 'package:app_kanu_delivery/app/constants.dart';
 import 'package:app_kanu_delivery/app/pages/home/widgets/body.dart';
-import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
-  // This widget is the root of your application.
+class HomeScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State {
-  int currentIndex;
-
-  @override
-  void initState() {
-    super.initState();
-
-    currentIndex = 0;
-  }
-
-  changePage(int index) {
-    setState(() {
-      currentIndex = index;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedItemIndex = 2;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: BubbleBottomBar(
-          opacity: 0.2,
-          backgroundColor: Colors.white,
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(16.0),
+    GestureDetector buildNavBarItem(IconData icon, int index) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            _selectedItemIndex = index;
+          });
+        },
+        child: Container(
+          width: MediaQuery.of(context).size.width / 5,
+          height: 60,
+          decoration: index == _selectedItemIndex
+              ? BoxDecoration(
+                  border:
+                      Border(bottom: BorderSide(width: 4, color: kGreenPea)),
+                  gradient: LinearGradient(colors: [
+                    kGreenPea.withOpacity(0.3),
+                    kGreenPea.withOpacity(0.016),
+                  ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+                )
+              : BoxDecoration(),
+          child: Icon(
+            icon,
+            color: index == _selectedItemIndex
+                ? kGreenPea
+                : Colors.black.withOpacity(0.6),
           ),
-          currentIndex: currentIndex,
-          hasInk: true,
-          inkColor: Colors.black12,
-          hasNotch: true,
-          fabLocation: BubbleBottomBarFabLocation.end,
-          onTap: changePage,
-          items: [
-            BubbleBottomBarItem(
-              backgroundColor: Colors.red,
-              icon: Icon(
-                Icons.dashboard,
-                color: Colors.black,
-              ),
-              activeIcon: Icon(
-                Icons.dashboard,
-                color: Colors.red,
-              ),
-              title: Text('Home'),
-            ),
-            BubbleBottomBarItem(
-              backgroundColor: Colors.indigo,
-              icon: Icon(
-                Icons.folder_open,
-                color: Colors.black,
-              ),
-              activeIcon: Icon(
-                Icons.folder_open,
-                color: Colors.indigo,
-              ),
-              title: Text('Folders'),
-            ),
-            BubbleBottomBarItem(
-              backgroundColor: Colors.deepPurple,
-              icon: Icon(
-                Icons.access_time,
-                color: Colors.black,
-              ),
-              activeIcon: Icon(
-                Icons.access_time,
-                color: Colors.deepPurple,
-              ),
-              title: Text('Log'),
-            ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      bottomNavigationBar: SafeArea(
+        child: Row(
+          children: [
+            buildNavBarItem(FontAwesomeIcons.sistrix, 0),
+            buildNavBarItem(FontAwesomeIcons.heart, 1),
+            buildNavBarItem(FontAwesomeIcons.home, 2),
+            buildNavBarItem(FontAwesomeIcons.commentAlt, 3),
+            buildNavBarItem(FontAwesomeIcons.user, 4),
           ],
         ),
-        body: SafeArea(child: Body()));
+      ),
+      body: SafeArea(
+        child: Body(),
+      ),
+    );
   }
 }
